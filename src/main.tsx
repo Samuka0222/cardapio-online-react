61fd0b7551c9d5ebb3851a0e18965b883c365567
Samuel Machado <mmachado0222@gmail.com>
2024-01-22 13:41:11 -0300

    Correção de import

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import AppRouter from './routes.tsx'
 import "./index.css"
 import { CardapioProvider } from './context/CardapioContext.tsx'
-import { CarrinhoProvider } from './context/contextoPedido/CarrinhoContext.tsx'
-import { EnderecoProvider } from './context/contextoPedido/EnderecoContext.tsx'
+import { CarrinhoProvider } from './context/CarrinhoContext.tsx'
+import { EnderecoProvider } from './context/EnderecoContext.tsx'
 import { MensagemProvider } from './context/MensagemContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
